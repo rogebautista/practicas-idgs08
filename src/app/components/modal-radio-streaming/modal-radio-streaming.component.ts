999befe8036fd29052294712bb1372538e169306
Rogelio Bautista Sanchez <rogeliobautistasanchez@MacBook-Air-de-Rogelio.local>
2024-11-21 17:52:12 -0600

    testing para streaming en pwa y native

--- src/app/components/modal-radio-streaming/modal-radio-streaming.component.ts
+++ src/app/components/modal-radio-streaming/modal-radio-streaming.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Capacitor } from '@capacitor/core';
+import { NativeAudio } from '@capacitor-community/native-audio';
+
 @Component({
   selector: 'app-modal-radio-streaming',
   templateUrl: './modal-radio-streaming.component.html',
@@ -10,13 +12,33 @@ export class ModalRadioStreamingComponent {
 
   streamUrl = 'http://24363.live.streamtheworld.com:3690/XEPHAM_SC';
   audio: HTMLAudioElement | null = null;
+  audioId = 'radioStream';
 
   constructor(private modalController: ModalController) {}
 
-  playRadio() {
+  async preloadAudio() {
+    if (Capacitor.isNativePlatform()) {
+      try {
+        await NativeAudio.preload({
+          assetId: this.audioId,
+          assetPath: this.streamUrl,
+          volume: 1.0,
+          isUrl: true
+        });
+      } catch (error) {
+        console.error('Error preloading audio:', error);
+      }
+    }
+  }
+  async playRadio() {
     if (Capacitor.isNativePlatform()) {
       // Optionally use Capacitor Media plugin
       console.log('Media plugin can be configured here');
+      try {
+        await NativeAudio.play({ assetId: this.audioId });
+      } catch (error) {
+        console.error('Error playing radio:', error);
+      }
     } else {
       // Use HTML5 audio for browser
       if (!this.audio) {
@@ -26,8 +48,15 @@ export class ModalRadioStreamingComponent {
     }
   }
 
-  stopRadio() {
-    if (this.audio) {
+  async stopRadio() {
+    if (Capacitor.isNativePlatform()){
+      try {
+        await NativeAudio.stop({ assetId: this.audioId });
+      } catch (error) {
+        console.error('Error stopping radio:', error);
+      }
+    }
+    else if (this.audio) {
       this.audio.pause();
       this.audio = null;
     }
